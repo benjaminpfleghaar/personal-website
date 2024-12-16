7e81459aecdda25ebe5cb0bfbab8a942bb923211
Benjamin Pfleghaar <info@benjaminpfleghaar.de>
2024-12-16 16:56:18 +0100

    adjust breakpoint

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,17 @@ import MatterEngine from "@/components/MatterEngine";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col gap-4 md:min-h-[calc(100dvh-6rem)] md:justify-between">
+		<div className="flex flex-col gap-4 sm:min-h-[calc(100dvh-6rem)] sm:justify-between">
 			<header>
 				<h1 className="text-2xl text-darkgray dark:text-lightgray">Benjamin Pfleghaar.</h1>
-				<p className="text-2xl">Web Developer with a strong back&shy;ground in UX Design. Open to new projects starting January 2025.</p>
+				<p className="text-2xl">Web Developer with a strong back&shy;ground in UX Design. Open to new pro&shy;jects starting January 2025.</p>
 			</header>
 			<main>
 				<MatterEngine />
 			</main>
 			<footer>
 				<nav aria-label="Legal Links">
-					<ul className="flex flex-col md:flex-row md:gap-6">
+					<ul className="flex flex-col sm:flex-row sm:gap-6">
 						<li>
 							<TextLink href="/privacy-policy">Privacy Policy</TextLink>
 						</li>
